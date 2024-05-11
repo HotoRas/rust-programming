5a8f80bf3bd19b3fcd021ee415836bcb05af1fb9
Hoto Ras <hotoras03@gmail.com>
2024-05-11 09:25:20 +0900

    guessing_game: Get user input and random so compare

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,7 +1,15 @@
 use std::io;
+use std::cmp::Ordering
+
+// cargo
+use rand::Rng;
+// end cargo
 
 fn main() {
     println!("Guess the number");
+
+    let secret_number = rand::thread_rng().gen_range(1..=100); // range exp: start..=end
+    //println!("Secret: {secret_number}")
 
     println!("Please input your guess:");
 
@@ -9,6 +17,13 @@ fn main() {
     io::stdin()
         .read_line(&mut guess) // insert to mutable var.
         .expect("Failed to read line");
+    let guess: u32 /*uint32*/ = guess.trim().parse().expect("Please type a number!");
     
     println!("You guessed: {guess}");
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You Win!"),
+    }
 }
