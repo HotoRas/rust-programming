faf390343c0e4a1e0fe8525eafa06a1a1274413d
Hoto Ras <hotoras03@gmail.com>
2024-05-11 09:27:25 +0900

    guessing_game: Do loop

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -11,19 +11,26 @@ fn main() {
     let secret_number = rand::thread_rng().gen_range(1..=100); // range exp: start..=end
     //println!("Secret: {secret_number}")
 
-    println!("Please input your guess:");
+    loop {
+        println!("Please input your guess:");
 
     let mut /* modifiable */ guess = String::new();
     io::stdin()
         .read_line(&mut guess) // insert to mutable var.
         .expect("Failed to read line");
-    let guess: u32 /*uint32*/ = guess.trim().parse().expect("Please type a number!");
+    let guess: u32 /*uint32*/ = guess.trim().parse() {
+        Ok(num) => num,
+        Err(_) => continue,
+    }
     
     println!("You guessed: {guess}");
 
     match guess.cmp(&secret_number) {
         Ordering::Less => println!("Too small!"),
         Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You Win!"),
+        Ordering::Equal => {
+            println!("You Win!"); break;
+        },
     }
+}
 }
